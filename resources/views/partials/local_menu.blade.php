ca0293fd35eb9a6ac2cfde77fcd18f18961cd061
Shem Pasamba <shemgp@yahoo.com>
2018-09-12 14:23:58 +0800

    fix: remove colon from menu

--- resources/views/partials/local_menu.blade.php
+++ resources/views/partials/local_menu.blade.php
@@ -4,7 +4,7 @@
             <span>{{ $title ?? 'Menu' }}</span>
         <i class="fa fa-angle-left pull-right"></i>
     </a>
-    @if (trim(view('layouts.menu')) != ''):
+    @if (trim(view('layouts.menu')) != '')
         <ul class="treeview-menu">
             @include('layouts.menu')
         </ul>
