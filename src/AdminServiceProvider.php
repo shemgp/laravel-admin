0f1f89fbc6a614e1902d1023bd6d95f6617e850e
Shem Pasamba <shemgp@yahoo.com>
2018-09-13 13:09:55 +0800

    don't override menu.blade.php if it exists already

--- src/AdminServiceProvider.php
+++ src/AdminServiceProvider.php
@@ -72,7 +72,8 @@ class AdminServiceProvider extends ServiceProvider
             $this->publishes([__DIR__.'/../resources/assets' => public_path('vendor/laravel-admin')], 'laravel-admin-assets');
 
             $this->publishes([__DIR__.'/../resources/views/partials/local_menu.blade.php' => resource_path('views/partials/local_menu.blade.php')], 'laravel-admin-local-menu');
-            $this->publishes([__DIR__.'/../resources/views/partials/local_menu_items.blade.php' => resource_path('views/layouts/menu.blade.php')], 'laravel-admin-local-menu-items');
+            if (!file_exists(resource_path('views/layouts/menu.blade.php')))
+                $this->publishes([__DIR__.'/../resources/views/partials/local_menu_items.blade.php' => resource_path('views/layouts/menu.blade.php')], 'laravel-admin-local-menu-items');
 
         }
 
